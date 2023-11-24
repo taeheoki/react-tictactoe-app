9e4d1feb5a341890b1fd99f8c8da06b4ba956946
taeheoki <dkqjwl2365@naver.com>
2023-11-25 01:16:53 +0900

    props를 생성할 때 배웠음

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Board from "./components/Board";
+import Test from "./components/Test";
 
 function App() {
   return (
     <div className="game">
+      <Test aprops="A" />
       <div className="game-board">
         <Board />
       </div>
--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,7 @@ import "./Board.css";
 
 export default class Board extends Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -17,14 +17,14 @@ export default class Board extends Component {
           {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
